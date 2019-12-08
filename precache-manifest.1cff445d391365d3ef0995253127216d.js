62b7c1e0db003cee040581d4486e02376623352d
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-12-08 11:44:33 +0100

    Updates

---
+++ precache-manifest.1cff445d391365d3ef0995253127216d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92095369726c4982dc68034ba23618e",
+    "url": "/toDoApp/index.html"
+  },
+  {
+    "revision": "d0791535885f97bc457f",
+    "url": "/toDoApp/static/css/main.5f15d7e9.chunk.css"
+  },
+  {
+    "revision": "93f24b734c3d77509dd2",
+    "url": "/toDoApp/static/js/2.c0c2bb90.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/toDoApp/static/js/2.c0c2bb90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0791535885f97bc457f",
+    "url": "/toDoApp/static/js/main.4d3ae00d.chunk.js"
+  },
+  {
+    "revision": "7a8905f1cf4a1a5ce0e0",
+    "url": "/toDoApp/static/js/runtime-main.d3ffb546.js"
+  }
+]);
